a9814032507feb50e77919ffea3f1b07d53d620e
Sean-Hassett <hassetse@tcd.ie>
2018-11-06 17:08:19 +0000

    small changes

--- ghvisual/lets.go
+++ ghvisual/lets.go
@@ -39,14 +39,14 @@ func main() {
 	fmt.Println(data.Repos_url)
 }
 
-var myClient = &http.Client{Timeout: 10 * time.Second}
-
 // Makes a HTTP GET request to the passed in URL. Parses JSON data from the body of the response
 // and writes matching entries to a passed in struct.
 func GetJson(config Configuration, url string, target interface{}) error {
+	var myClient = &http.Client{Timeout: 10 * time.Second}
+
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 	request.Header.Set("User-Agent", userAgent)
 	request.SetBasicAuth(config.Username, config.Token)
