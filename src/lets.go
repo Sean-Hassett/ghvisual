6cb836f095aa0cc712d87532d9da6519b8957a7e
Sean-Hassett <hassetse@tcd.ie>
2018-11-06 14:32:16 +0000

    added func to write selected json to a struct

--- src/lets.go
+++ src/lets.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
+const userAgent = "Sean-Hassett_ghvisual"
+const addr = "https://api.github.com"
+
+type Json struct {
+	Current_user_url string
+	Starred_url      string
+}
+
 func main() {
-	response, err := http.Get("https://api.github.com")
+	data := Json{}
+	getJson(addr, &data)
+	fmt.Println(data.Current_user_url)
+	fmt.Println(data.Starred_url)
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
+		fmt.Println(err)
 	}
+	request.Header.Set("User-Agent", userAgent)
+
+	response, err := myClient.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer response.Body.Close()
+	return json.NewDecoder(response.Body).Decode(target)
 }
