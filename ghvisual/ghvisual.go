db15974830c9eea36cb663f9d303d0e9eb0aba09
Sean-Hassett <hassetse@tcd.ie>
2018-11-30 11:04:30 +0000

    started activeDays chart

--- ghvisual/ghvisual.go
+++ ghvisual/ghvisual.go
@@ -24,15 +24,10 @@ func main() {
 	}
 }
 
-// simple rounding function
-func Round(x, unit float64) float64 {
-	return math.Round(x/unit) * unit
-}
-
-// normalise the daysSinceUpdate value for each repo into the range specified by the upper and lower bounds
-func normaliseDaysSinceUpdate(days []int, lower, upper int) []int {
-	min, max := days[0], days[0]
-	for _, day := range days {
+// normalise a list of values into the range specified by the upper and lower bounds
+func normaliseRange(list_in []int, lower, upper int) []int {
+	min, max := list_in[0], list_in[0]
+	for _, day := range list_in {
 		if day > max {
 			max = day
 		}
@@ -42,8 +37,8 @@ func normaliseDaysSinceUpdate(days []int, lower, upper int) []int {
 	}
 
 	var normalisedDays = []int{}
-	for _, day := range days {
-		normalisedDays = append(normalisedDays, int(Round(float64(lower+(day-min)*(upper-lower)/(max-min)), 5)))
+	for _, day := range list_in {
+		normalisedDays = append(normalisedDays, int(float64(lower+(day-min)*(upper-lower)/(max-min))))
 	}
 	return normalisedDays
 }
@@ -81,26 +76,24 @@ func draw(w http.ResponseWriter, req *http.Request) {
 	userColor := []int{255, 192, 76}
 
 	// map number of commits per day
-	var activeDays = map[string]int{
-		"Monday":    0,
-		"Tuesday":   0,
-		"Wednesday": 0,
-		"Thursday":  0,
-		"Friday":    0,
-		"Saturday":  0,
-		"Sunday":    0,
-	}
+	var activeDays = []int{0, 0, 0, 0, 0, 0, 0}
+	Monday := 0
+	Tuesday := 1
+	Wednesday := 2
+	Thursday := 3
+	Friday := 4
+	Saturday := 5
+	Sunday := 6
 	// map number of commits to time of day
 	// Morning: 6:00am-11:59am
 	// Afternoon: 12:00pm-5:59pm
 	// Evening: 6:00pm-11:59pm
 	// Night: 12:00am-5:59am
-	var activeHours = map[string]int{
-		"Morning":   0,
-		"Afternoon": 0,
-		"Evening":   0,
-		"Night":     0,
-	}
+	var activeHours = []int{0, 0, 0, 0}
+	Morning := 0
+	Afternoon := 1
+	Evening := 2
+	Night := 3
 
 	var daysSinceUpdate []int
 	var diameters []float64
@@ -112,30 +105,30 @@ func draw(w http.ResponseWriter, req *http.Request) {
 		for _, commit := range repo.Commits {
 			switch commit.Date.Weekday() {
 			case time.Monday:
-				activeDays["Monday"] += 1
+				activeDays[Monday] += 1
 			case time.Tuesday:
-				activeDays["Tuesday"] += 1
+				activeDays[Tuesday] += 1
 			case time.Wednesday:
-				activeDays["Wednesday"] += 1
+				activeDays[Wednesday] += 1
 			case time.Thursday:
-				activeDays["Thursday"] += 1
+				activeDays[Thursday] += 1
 			case time.Friday:
-				activeDays["Friday"] += 1
+				activeDays[Friday] += 1
 			case time.Saturday:
-				activeDays["Saturday"] += 1
+				activeDays[Saturday] += 1
 			case time.Sunday:
-				activeDays["Sunday"] += 1
+				activeDays[Sunday] += 1
 			}
 			hour := commit.Date.Hour()
 			switch {
 			case hour < 6:
-				activeHours["Night"] += 1
+				activeHours[Night] += 1
 			case hour < 12:
-				activeHours["Morning"] += 1
+				activeHours[Morning] += 1
 			case hour < 18:
-				activeHours["Afternoon"] += 1
+				activeHours[Afternoon] += 1
 			case hour < 24:
-				activeHours["Evening"] += 1
+				activeHours[Evening] += 1
 			}
 		}
 		diameter := math.Log(float64(repo.Size))*10*2
@@ -143,23 +136,26 @@ func draw(w http.ResponseWriter, req *http.Request) {
 		sumOfDiameters += int(diameter + buffer)
 	}
 
-	normalisedDays := normaliseDaysSinceUpdate(daysSinceUpdate, 5, 60)
+	normalisedDays := normaliseRange(daysSinceUpdate, 5, 60)
 	radius = float64(sumOfDiameters) / (2.0 * math.Pi)
 	theta := -(buffer / float64(sumOfDiameters) * degrees)
+	minX := width
 
 	for i, repo := range repoList {
 		brightnessMult := float64(normalisedDays[i]) / 60.0
 		circleColor := redistributeRGB([]int{int(repoColor[0] * brightnessMult), int(repoColor[1] * brightnessMult), int(repoColor[2] * brightnessMult)})
 		s := canvas.RGB(int(circleColor[0]), int(circleColor[1]), int(circleColor[2]))
-
 		//angle in radians
 		offset := buffer / float64(sumOfDiameters) * degrees
 		theta += offset/2 + (diameters[i] / 2 / float64(sumOfDiameters) * degrees)
 		angle := theta * degreeToRadian
 
 		xVal := int(radius*math.Cos(angle)+width/2)
+		if xVal - int(diameters[i] / 2) < minX {
+			minX = xVal- int(diameters[i] / 2)
+		}
 		yVal := int(radius*math.Sin(angle)+height/2)
-		canvas.Line(xVal, yVal, width/2, height/2, "fill:black:weight:2")
+		canvas.Line(xVal, yVal, width/2, height/2, "stroke:black")
 		canvas.Circle(xVal, yVal, int((math.Log(float64(repo.Size)))*10), s)
 		canvas.Text(xVal, yVal+4, strconv.Itoa(i+1), "fill:white;text-anchor:middle")
 
@@ -168,6 +164,24 @@ func draw(w http.ResponseWriter, req *http.Request) {
 	}
 	canvas.Circle(width/2, height/2, int(radius/2), canvas.RGB(userColor[0], userColor[1], userColor[2]))
 	canvas.Text(width/2, height/2+20, config.Username, "fill:black;text-anchor:middle;font-size:40px")
-	canvas.Line(0, height/2, width, height/2)
+
+	chartBuffer := 50
+	chartXStart := chartBuffer
+	chartYStart := chartBuffer
+	chartWidth := minX - chartBuffer*2
+	chartHeight := height/2 - (chartBuffer)
+	space := 10
+
+	dayBarWidth := (chartWidth-space) / 7 - space
+	canvas.Rect(chartXStart, chartYStart, chartWidth, chartHeight, "fill:rgb(140,140,140)")
+	activeDaysValues := []int{}
+	for _, day := range activeDays{
+		activeDaysValues = append(activeDaysValues, day)
+	}
+	normalActiveDaysValues := normaliseRange(activeDaysValues, 100, chartHeight-chartBuffer)
+	for i := range normalActiveDaysValues {
+		p := ""
+		canvas.Rect(chartXStart+space+(dayBarWidth + space)*i, chartYStart+chartHeight-normalActiveDaysValues[i], dayBarWidth, normalActiveDaysValues[i], "fill:rgb(51,78,78")
+	}
 	canvas.End()
 }
